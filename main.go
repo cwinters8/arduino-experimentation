690b5fa59cc4fc03b64cc579f2e73adc6e775f08
Clark Winters <clwinters8@gmail.com>
2021-09-29 15:50:04 -0500

    tinygo version of blinking LED

--- main.go
+++ main.go
@@ -1,29 +1,17 @@
 package main
 
 import (
-	"flag"
-	"log"
+	"machine"
 	"time"
-
-	"gobot.io/x/gobot"
-	"gobot.io/x/gobot/drivers/gpio"
-	"gobot.io/x/gobot/platforms/firmata"
 )
 
 func main() {
-	device := flag.String("device", "", "Serial device path such as /dev/ttyACM0")
-	flag.Parse()
-
-	adaptor := firmata.NewAdaptor(*device)
-	led := gpio.NewLedDriver(adaptor, "13") // what does the 13 pin refer to?
-	work := func() {
-		gobot.Every(500*time.Millisecond, func() {
-			err := led.Toggle()
-			if err != nil {
-				log.Fatal("Failed to toggle LED: " + err.Error())
-			}
-		})
+	led := machine.LED
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	for {
+		led.High()
+		time.Sleep(time.Second)
+		led.Low()
+		time.Sleep(time.Second)
 	}
-	robot := gobot.NewRobot("bot", []gobot.Connection{adaptor}, []gobot.Device{led}, work)
-	robot.Start()
 }
